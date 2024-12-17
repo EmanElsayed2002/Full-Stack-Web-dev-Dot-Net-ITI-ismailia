af1262a40358a167c0fd2988173d093a06ba98da
EmanElsayed2002 <emoelsayed2192002@gmail.com>
2024-12-17 02:13:08 +0200

    upload assosiation solution Eman Elsayed Done

---
+++ C#/Lab03/Lab03 - (Eman Elsayed)/Assosiation/Complex.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+     class Complex
+    {
+        int real, img;
+
+        public Complex()
+        {
+            real = img = 0;
+            Console.WriteLine("Ctor created");
+        }
+        public Complex(int real, int img)
+        {
+            this.real = real;
+            this.img = img;
+        }
+        public int getReal() { return real; }
+        public int getImg() { return img; }
+
+        public string Print()
+        {
+
+
+            if (real > 0)
+            {
+                if (img > 1)
+                {
+                    return $"{real} + {img}i";
+                }
+                else if (img == 1)
+                {
+                    return $"{real} + i";
+                }
+                else if (img == -1)
+                {
+                    return $"{real} -i";
+                }
+                else if (img == 0)
+                {
+                    return $"{real}";
+                }
+                else
+                {
+                    return $"{real} {img}i";
+                }
+            }
+            else if (real == 0)
+            {
+                if (img > 1)
+                {
+                    return $"{img}i";
+                }
+                else if (img == 0)
+                {
+                    return "0";
+                }
+                else if (img == 1)
+                {
+                    return $"i";
+                }
+                else if (img == -1)
+                {
+                    return $"-i";
+                }
+                else
+                {
+                    return $" {img}i";
+                }
+            }
+            else
+            {
+                if (img > 1)
+                {
+                    return $"{real} + {img}i";
+                }
+                else if (img == 1)
+                {
+                    return $"{real} + i";
+                }
+                else if (img == 0)
+                {
+                    return $"{real}";
+                }
+                else if (img == -1)
+                {
+                    return $"{real} - i";
+                }
+                else
+                {
+                    return $"{real}  {img}i";
+                }
+            }
+        }
+
+    }
+}
---
+++ C#/Lab03/Lab03 - (Eman Elsayed)/Assosiation/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    class Employee
+    {
+        int ssn, age;
+        string name;
+        public Employee()
+        {
+            ssn = 0;
+            age = 0;
+            name = null;
+            Console.WriteLine("Employee Constructor Created");
+        }
+
+        public Employee(int ssn, int age, string name)
+        {
+            this.ssn = ssn;
+            this.age = age;
+            this.name = name;
+        }
+
+        public string Print(Department dept , HireDate hiredate)
+        {
+            return $"Employee Name: {name}\tAge: {age}\tSSN: {ssn}\t{dept.Print()}\thireDate: {hiredate.Print()}";
+        }
+
+
+    }
+}
