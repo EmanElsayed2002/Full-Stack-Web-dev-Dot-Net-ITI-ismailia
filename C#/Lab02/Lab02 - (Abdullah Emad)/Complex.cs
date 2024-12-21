44ed4eeaa8691f055726535d2f8b3eb9ca98d4c3
AbdullahEmad07 <abdullah.emad237@gmail.com>
2024-12-21 18:39:38 +0200

    Add files via upload

---
+++ C#/Lab02/Lab02 - (Abdullah Emad)/Complex.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Complex
+	{
+		private int real;
+		private int img;
+
+		public Complex() 
+		{
+			real = 0;
+			img = 0;
+		}
+		public Complex(int _real,int _img)
+		{
+			real = _real;
+			img = _img;
+		}
+		public void SetReal(int _real)
+		{
+			real = _real;
+		}
+		public int GetReal() { return real; }
+		public void SetImg(int _img) { img = _img; }
+		public int GetImg() { return img; }
+
+		public string Print()
+		{
+			if(real==0 && img==0)
+			{
+				return $"0";
+			}
+			else if(real==0 && img == 1)
+			{
+				return $"i";
+			}
+			else if(real == 0 && img == -1)
+			{
+				return $"-i";
+			}
+			else if(real==0)
+			{
+				return $"{img}i";
+			}
+			else if (real != 0 && img == 0)
+			{
+				return $"{real}";
+			}
+			else if (real != 0 && img == 1)
+			{
+				return $"{real} + i";
+			}
+			else if(real!=0 && img==-1)
+			{
+				return $"{real} - i";
+			}
+			else if(real != 0 && img >0)
+			{
+				return $"{real} + {img}i";
+			}
+			else if(real != 0 && img < 0)
+			{
+				return $"{real} - {img*-1}i";
+			}
+
+			return $"{real} + {img}i";
+
+		}
+
+		public Complex Add(Complex right)
+		{
+			Complex result = new Complex();
+			result.real =real + right.real;
+			result.img =img + right.img;
+			return result;
+		}
+	}
+}
---
+++ C#/Lab02/Lab02 - (Abdullah Emad)/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Employee
+	{
+		private int id;
+		private string name;
+		private int age;
+		private decimal salary;
+		private HireDate date;
+		private Department department;
+
+		
+
+		public void SetId(int _id)
+		{
+			id = _id;
+		}
+		public int GetId()
+		{
+			return id;
+		}
+		public void SetName(string _name)
+		{
+			name = _name;
+		}
+		public string GetName()
+		{
+			return name;
+		}
+
+		public void SetAge(int _age)
+		{
+			age = _age;
+		}
+		public int GetAge()
+		{ 
+			return age;
+		}
+
+		public void SetSalary(decimal _salary)
+		{
+			salary= _salary;
+		}
+
+		public decimal GetSalary()
+		{
+			return salary;
+		}
+		public void SetHireDate(HireDate _date)
+		{
+			date = _date;
+		}
+
+		public HireDate GetHireDate()
+		{
+			return date;
+		}
+
+		public void SetDepartment(Department _department)
+		{
+			department = _department;
+		}
+
+		public Department GetDepartment()
+		{
+			return department;
+		}
+
+		public string print()
+		{
+			return $"{id}:{name}:{age}:{salary}:{date.print()}:{department.print()}";
+		}
+	}
+}
