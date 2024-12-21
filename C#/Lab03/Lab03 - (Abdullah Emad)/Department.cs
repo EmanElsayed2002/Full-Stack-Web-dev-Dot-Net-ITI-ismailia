dc465cb8864d3147bef8a89777695706696eaa63
AbdullahEmad07 <abdullah.emad237@gmail.com>
2024-12-21 17:48:11 +0200

    Add files via upload

---
+++ C#/Lab03/Lab03 - (Abdullah Emad)/Department.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Department
+	{
+		private int deptId;
+		private string deptName;
+
+		public Department()
+		{
+
+		}
+
+		public void SetDeptId(int _deptId)
+		{
+			deptId = _deptId;
+		}
+		public int GetDeptId()
+		{
+			return deptId;
+		}
+		public void SetDeptName(string _deptName)
+		{
+			deptName = _deptName;
+		}
+		public string GetDeptName()
+		{
+			return deptName;
+		}
+
+		public string print()
+		{
+			return deptName;
+		}
+	}
+}
