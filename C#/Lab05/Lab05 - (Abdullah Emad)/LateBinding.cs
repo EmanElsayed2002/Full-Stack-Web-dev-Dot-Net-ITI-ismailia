26cd96cd3df7a5175c1459c4c03f8c218630c0f1
Abdullah Emad <abdullah.emad237@gmail.com>
2024-12-22 17:38:35 +0200

    solution of Lab05

---
+++ C#/Lab05/Lab05 - (Abdullah Emad)/LateBinding.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05
+{
+	class Parent
+	{
+		protected int x;
+		protected int y;
+		public void SetX(int _x) { x = _x; }
+		public void SetY(int _y) { y = _y; }
+		public int getX() { return x; }
+		public int getY() { return y; }
+
+		public Parent()
+		{
+			x = 1;
+			y = 2;
+		}
+
+		public virtual void Show()
+		{
+			Console.WriteLine("I am parent");
+		}
+
+	}
+
+	class Child : Parent
+	{
+		protected int z;
+		public void SetZ(int _z) { z = _z; }
+		public int GetZ() { return z; }
+
+		public Child()
+		{
+			x = y = z = 0;
+		}
+		public Child(int _x, int _y, int _z)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+		}
+
+		public override void Show()
+		{
+			Console.WriteLine("I am child");
+		}
+	}
+
+	class SubChild : Child
+	{
+		protected int n;
+		public void SetN(int _n) { n = _n; }
+		public int GetN() { return n; }
+
+		public SubChild()
+		{
+			x = y = z = n = 0;
+		}
+
+		public override void Show()
+		{
+			Console.WriteLine("I am subchild");
+		}
+
+
+	}
+}
