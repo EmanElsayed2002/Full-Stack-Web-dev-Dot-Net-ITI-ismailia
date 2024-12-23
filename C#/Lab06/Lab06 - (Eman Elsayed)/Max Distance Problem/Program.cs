acd5dd2d851ab96635667c3866604659d51b2a90
EmanElsayed2002 <emoelsayed2192002@gmail.com>
2024-12-23 12:35:45 +0200

    make Lab06 and push first task Max Distance

---
+++ C#/Lab06/Lab06 - (Eman Elsayed)/Max Distance Problem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Size OF Your Array: ");
+            int n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+            int maxDis = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter Element #{i + 1} : ");
+
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        maxDis = int.Max(maxDis, j - i - 1);
+                    }
+                }
+            }
+            Console.WriteLine($"Max Distance amoung Array is : {maxDis}");
+        }
+    }
+}
