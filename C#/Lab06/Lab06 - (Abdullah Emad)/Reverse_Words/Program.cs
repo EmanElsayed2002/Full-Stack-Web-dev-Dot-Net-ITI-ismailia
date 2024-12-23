a6717df5167d6f86e7996ece57280071167d7e1e
Abdullah Emad <abdullah.emad237@gmail.com>
2024-12-23 14:58:37 +0200

    solution of Lab06

--- C#/Lab06/Lab06 - (Abdullah Emad)/Reverse_Words/Program.cs
+++ C#/Lab06/Lab06 - (Abdullah Emad)/Reverse_Words/Program.cs
@@ -1,1 +1,9 @@
+Console.WriteLine("Enter a sentence");
+string str = Console.ReadLine();
+string[] words = str.Split(' ');
+for (int i = words.Length - 1; i >= 0; i--)
+{
+	Console.Write($"{words[i]} ");
 
+}
+Console.WriteLine();
