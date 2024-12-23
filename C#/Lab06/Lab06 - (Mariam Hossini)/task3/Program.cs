45e8fcbb3160a588b177c0426d64dd397cdb4409
MariamHosini <hossinimariam93@gmail.com>
2024-12-23 15:55:55 +0200

    add slution to lab6

---
+++ C#/Lab06/Lab06 - (Mariam Hossini)/task3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch s1 = new Stopwatch();
+            Stopwatch s2 = new Stopwatch();
+            Stopwatch s3 = new Stopwatch();
+
+            int num;
+            Console.Write("Enter the number: ");
+            num = int.Parse(Console.ReadLine());
+
+            //count the number of 1s
+
+            #region FIRST METHOD    
+            string number;
+            int count = 0;
+            s1.Start();
+            for (int i = 0; i < num; i++)
+            {
+                number = Convert.ToString(i);
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (number[j] == '1') { count++; }
+                }
+            }
+            s1.Stop();
+            Console.WriteLine(count);
+            Console.WriteLine(s1.Elapsed);
+            #endregion
+
+            #region SECOND METHOD
+
+            int count2 = 0;
+            s2.Start();
+            for (int i = 0; i < num; i++) {
+                int number2 = i;
+                while (number2 > 0)
+                {
+                    if (number2 % 10 == 1) {
+                        count2++;
+                    }
+                    number2 /= 10;
+
+                } 
+            }
+            Console.WriteLine(count2);
+            Console.WriteLine(s2.Elapsed);
+            #endregion  
+        }
+    }
+}
