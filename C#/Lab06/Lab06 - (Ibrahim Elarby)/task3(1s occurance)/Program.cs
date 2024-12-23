85f44b295ba44b76f732f7c74c8c1a6509fde312
unknown <Ibrahim.elarby.scurt@gmail.com>
2024-12-23 15:35:08 +0200

    lab06

---
+++ C#/Lab06/Lab06 - (Ibrahim Elarby)/task3(1s occurance)/Program.cs
@@ -0,0 +1,14 @@
+﻿using task2.task2;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr2 = "my name is ibrahim";
+
+            Console.WriteLine(string.Join(" ",arr2.Split(" ").Reverse()));
+        }
+    }
+}
