65f16d56a1df66039b8597edbc3974ee5ac0d018
Abdullah Emad <abdullah.emad237@gmail.com>
2024-12-24 15:47:59 +0200

    solution of Lab07

---
+++ C#/Lab07/Lab07 - (Abdullah Emad)/Enumeration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07
+{
+	public enum Gender
+	{
+		male,
+		female
+	}
+	
+	public enum SecurityLevel
+	{
+		guest,
+		developer,
+		secretary,
+		DBA
+	}
+}
