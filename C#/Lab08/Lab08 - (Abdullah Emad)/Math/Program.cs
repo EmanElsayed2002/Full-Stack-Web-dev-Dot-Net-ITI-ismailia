3df957d4eb855527039f1a8559d86026669b97a8
Abdullah Emad <abdullah.emad237@gmail.com>
2024-12-25 16:40:35 +0200

    Update Program.cs

--- C#/Lab08/Lab08 - (Abdullah Emad)/Math/Program.cs
+++ C#/Lab08/Lab08 - (Abdullah Emad)/Math/Program.cs
@@ -1,1 +1,13 @@
+using MathProgram;
+using Math = MathProgram.Math;
+
+Math obj = Math.createObject();
+int result = 0;
+result = Math.Add(3, 3);
+//result =obj.Add(2,3);
+Console.WriteLine(result);
+
+result = obj.Multiply(3,4);
+Console.WriteLine(result);
+
 
